8cc16103918e10697ce04bba3aa1df43a811a4f9
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2023-11-08 01:08:51 -0500

    Create lesson_4.py

---
+++ Python_Basic/lesson_4.py
@@ -0,0 +1,21 @@
+var_1 = 7
+print(var_1)
+
+if var_1 >= 5:
+    print("var_1 more than 5")
+else:
+    print("var_1 less or equal 5")
+
+print("it's last line")
+# >, <, >=, <=, ==, !=
+
+
+secret = input("Please enter secret phrase: ")
+
+if secret == "python":
+    print("Welcome")
+else:
+    print("You are wrong")
+
+print("Have a good day")
+
