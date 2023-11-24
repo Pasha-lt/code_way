8c76924424926512a8de7c823129eaa23a155959
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2023-11-24 00:11:52 -0500

    Create lesson_6.py

---
+++ Python_Basic/lesson_6.py
@@ -0,0 +1,19 @@
+# code_way: subscribe, comment, like
+
+# for i in [1, 2, 3]:
+#     print(i)
+#     input()
+#
+# print("new line")
+
+# for i in "Hello Python":
+#     print(i)
+
+list_of_numbers = [-99, 100, 5, 20, -17]
+
+for i in list_of_numbers:
+    if i > 0:
+        print(i / 4)
+    else:
+        print(i + 20)
+
