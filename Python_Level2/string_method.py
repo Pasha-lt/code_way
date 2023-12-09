b2e3f35ee388af7570d31bb46f7279bee4c553fd
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2023-12-09 01:34:30 -0500

    Create string_method.py

---
+++ Python_Level2/string_method.py
@@ -0,0 +1,28 @@
+our_string = "Hello world"
+
+# print(our_string.upper())  # HELLO WORLD
+# print(our_string.lower())  # hello world
+# print(our_string.count("l"))  # 3
+# print(our_string.count("ll"))  # 1
+# print(our_string.count("l", 3))  # 2
+# print(our_string.count("l", 3, 7))  # 1
+# print(our_string.find("wor"))  # 6
+# print(our_string.find("o"))  # 4
+# print(our_string.rfind("o"))  # 7
+# print(our_string.find("apple"))  # -1
+# print(our_string.index("apple"))  # ValueError: substring not found
+# print(our_string.replace("Hello", "Hi"))  # Hi world
+# print(our_string.replace(" ", ""))  # Helloworld
+# print(our_string.isalpha())  # False
+# print("fdsfds".isalpha())  # True
+# print("789".isdigit())  # True
+# print("text".rjust(10))  #       text
+# print("text".rjust(10, "!"))  # !!!!!!text
+# print("text".ljust(10, "1"))  # text111111
+new_string = "     hi      "
+# print(new_string.strip())  # hi
+# print(new_string.lstrip())  # hi
+# print(new_string.rstrip())  #      hi
+print(our_string)
+our_string = our_string.upper()
+print(our_string)
