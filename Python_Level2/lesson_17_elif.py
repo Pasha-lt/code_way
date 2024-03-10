dc942382277a0a6249c5d62a4097d516410b2b12
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2024-03-09 23:14:26 -0500

    Create lesson_17_elif.py

---
+++ Python_Level2/lesson_17_elif.py
@@ -0,0 +1,14 @@
+number = 4
+
+# if number > 0:
+#     print('positive number')
+# elif number < 0:
+#     print('negative number')
+# else:
+#     print('zero')
+
+if number > 0:
+    print('positive number')
+elif number % 2 == 0:
+    print('can be divided by two')
+
