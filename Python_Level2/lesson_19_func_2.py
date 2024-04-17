ed340708ed452d51ca8a8f69c42c5f7cddce6a8b
Pavel <55654045+Pasha-lt@users.noreply.github.com>
2024-04-17 12:36:17 -0400

    Create lesson_19_func_2.py

---
+++ Python_Level2/lesson_19_func_2.py
@@ -0,0 +1,9 @@
+def add(number_one, number_two):
+    result = number_one + number_two
+    return result
+
+def exp(number):
+    return number ** 2
+
+our_result = exp(add(3, 5))
+print(our_result)
